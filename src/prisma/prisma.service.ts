e5e0cbae6b0037b9121e8893bfeb7dcc921f6231
pinguimsurfante <gustavoprof22@gmail.com>
2026-01-07 11:09:48 -0300

    chore: stabilize prisma adapter and weather data models

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma';
+import { PrismaClient } from '@prisma/client';
+
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
