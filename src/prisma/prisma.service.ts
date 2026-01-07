96083250abd2717b8b270d11cf79d07e9da30c96
pinguimsurfante <gustavoprof22@gmail.com>
2026-01-07 11:14:30 -0300

    fix

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,20 +1,23 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
-
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit
+{
   constructor() {
-    const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL,
+    super({
+      adapter: new PrismaPg({
+        connectionString: process.env.DATABASE_URL!,
+      }),
     });
-    super({ adapter });
   }
 
   async onModuleInit() {
     try {
-      await this.$queryRaw`SELECT 1`;
+      await this.$connect(); // ✅ NÃO use $queryRaw
       Logger.log('Conectado ao database');
     } catch (error) {
       Logger.error('Não conectado ao database', error);
