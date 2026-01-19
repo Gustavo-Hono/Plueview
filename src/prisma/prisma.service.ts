34b356d62a7dae8d304863cb40e1fcd7cc7b98c6
Gustavo Seidi Hono <gustavo.hono@fatec.sp.gov.br>
2026-01-19 09:17:41 -0300

    fix: prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,33 +1,13 @@
-import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  private readonly logger = new Logger(PrismaService.name);
-
-  constructor() {
-    // Como você usa a porta 5432, criamos um pool para gerenciar a conexão direta
-    const pool = new Pool({ 
-      connectionString: process.env.DATABASE_URL,
-      max: 10 // Limite de conexões simultâneas para não estourar o Supabase Free
-    });
-    
-    const adapter = new PrismaPg(pool);
-
-    // OBRIGATÓRIO na v7 quando o schema.prisma não tem a propriedade 'url'
-    super({ adapter });
-  }
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy {
 
   async onModuleInit() {
-    try {
-      await this.$connect();
-      this.logger.log('✅ Conexão direta estabelecida na porta 5432!');
-    } catch (error) {
-      this.logger.error('❌ Erro ao conectar ao Supabase:', error);
-      throw error;
-    }
+    await this.$connect();
   }
 
   async onModuleDestroy() {
